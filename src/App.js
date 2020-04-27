8ca47654378b7a07c5801f745f4d47b6ec757dd7
gsygh <gsygh@naver.com>
2020-04-27 15:45:59 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+import { Auth } from 'aws-amplify';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <header className="App-header"> />
+        <button onClick={() => Auth.federatedSignIn({provider: "Google"})}>sign in with google</button>
+        <button onClick={() => Auth.federatedSignIn()}>sign in</button>
       </header>
     </div>
-  );
+    );
+  }
 }
-
 export default App;
